bcd6fb8134bd966a6c40e25760c332414386c6b8
Roopesht <roopesht@gmail.com>
2019-08-04 20:24:44 +0530

    initial version

---
+++ Task1-PrimeNo/primeno.py
@@ -0,0 +1,3 @@
+import math
+def isPrime(num):
+	return False
---
+++ Task1-PrimeNo/primeno_test.py
@@ -0,0 +1,19 @@
+import unittest
+from primeno import isPrime
+
+class PrimeNoTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    def testcheckZero(self):
+        print('test')
+        self.assertEqual( isPrime(0), False)  
+
+    def testNum181(self):
+        print('test')
+        self.assertEqual( isPrime(181), True)  
+
+    def testNum180(self):
+        print('test')
+        self.assertEqual( isPrime(180), False)  
+if __name__ == '__main__': 
+    unittest.main()
